86cec8d40d8ef857bc4ffa92cef715d711c830ac
James Samotshozo <james.samotshozo@ngc.com>
2020-07-17 11:30:15 -0400

    Fixing deactivate modal button styling

--- src/components/DeactivateModal.tsx
+++ src/components/DeactivateModal.tsx
@@ -1,87 +1,94 @@
 import React from "react"
-import { Box,Flex, PseudoBox } from "@chakra-ui/core"
-import {
-	P,
-	Button,
-	ButtonSize,
-	H4,
-	Modal,
-	ModalBody,
-	ModalFooter,
-	ModalHeader,
-	ModalCloseButton,
-} from "@c1ds/components"
+import { Box, Flex, Button as ChakraButton } from "@chakra-ui/core"
+import { P, Button, ButtonSize, H4, Modal, ModalBody, ModalFooter, ModalHeader, ModalCloseButton } from "@c1ds/components"
 import { LinkButton } from "../components/LinkButton"
 
 interface DeactivateModalProps {
 	isOpen: boolean
 	onCancel: Modal["onClose"]
-    onConfirm: Modal["onClose"]
-    eventName?: string
-    isActive?: boolean
+	onConfirm: Modal["onClose"]
+	eventName?: string
+	isActive?: boolean
 }
 
-const DeactivateModal: React.FC<DeactivateModalProps> = ({isOpen, onCancel, onConfirm, eventName, isActive = undefined}: DeactivateModalProps) => {
+const buttonStyle = {
+	default: {
+		bg: "badge",
+		color: "white",
+		border: "none",
+	},
+	hoverFocus: {
+		borderStyle: "solid",
+		borderWidth: "2",
+		borderColor: "accent",
+	},
+	active: {
+		bg: "#a30014",
+		border: "none",
+	},
+}
 
+const DeactivateModal: React.FC<DeactivateModalProps> = ({
+	isOpen,
+	onCancel,
+	onConfirm,
+	eventName,
+	isActive = undefined,
+}: DeactivateModalProps) => {
 	// if eventName and isActive are not defined, it will prompt default Deactive Modal with no event information.
-    const status = typeof isActive === 'undefined' ? 'Deactivate' : isActive ? 'Deactivate' : 'Activate';
+	const status = typeof isActive === "undefined" ? "Deactivate" : isActive ? "Deactivate" : "Activate"
 
 	return (
-        <Modal isOpen={isOpen} onClose={onCancel} isCentered={true} size="sm">
-		<ModalHeader>
-			<H4>{status} Event?</H4>
-		</ModalHeader>
-		<ModalCloseButton />
-		<ModalBody>
-            <P>Are you sure you want to {status.toLocaleLowerCase()} {eventName??'this event'}?</P>
-		</ModalBody>
+		<Modal isOpen={isOpen} onClose={onCancel} isCentered={true} size="sm">
+			<ModalHeader>
+				<H4>{status} Event?</H4>
+			</ModalHeader>
+			<ModalCloseButton />
+			<ModalBody>
+				<P>
+					Are you sure you want to {status.toLocaleLowerCase()} {eventName ?? "this event"}?
+				</P>
+			</ModalBody>
 
-		<ModalFooter>
-			<Flex align="center">
-				<Box marginRight="20">
-					<LinkButton onClick={onCancel}>Cancel</LinkButton>
-				</Box>
-                {
-                    typeof isActive === 'undefined' ? 
-                    <Button size={ButtonSize.SM} onClick={onConfirm}>
-                        YES
-                    </Button> :
-					isActive ?
-					// Deactive button with warning background color. 
-					<PseudoBox
-						as="button"
-						textAlign="center"
-						fontFamily="body"
-						fontSize="button"
-						fontWeight="button"
-						lineHeight="normal"
-						px={20}
-						py={12}
-						bg="red"
-						color ="white"
-						border = "1px solid red"
-						outline='none'
-						_hover={{ 
-							borderStyle: "solid",
-							borderWidth: "1px",
-							borderColor: "accent",
-						}}
-						_active={{
-							bg: "required",
-							borderColor: "accent",
-						}}
-						onClick={onConfirm}>
-						{status}
-					</PseudoBox> :
-                    <Button size={ButtonSize.SM} onClick={onConfirm}>
-                        {status}
-                    </Button> 
-                }
-			</Flex>
-		</ModalFooter>
-	</Modal>
-    )
-	
+			<ModalFooter>
+				<Flex align="center">
+					<Box marginRight="20">
+						<LinkButton onClick={onCancel}>Cancel</LinkButton>
+					</Box>
+					{typeof isActive === "undefined" ? (
+						<Button size={ButtonSize.SM} onClick={onConfirm}>
+							YES
+						</Button>
+					) : isActive ? (
+						// Deactive button with warning background color.
+						<ChakraButton
+							height="input"
+							width="buttonMd"
+							textAlign="center"
+							borderRadius={0}
+							fontFamily="body"
+							fontSize="button"
+							fontWeight="button"
+							px={20}
+							py={12}
+							{...buttonStyle.default}
+							_focus={buttonStyle.hoverFocus}
+							_hover={buttonStyle.hoverFocus}
+							_active={buttonStyle.active}
+							onClick={onConfirm}>
+							<Box flex="1 1 0" lineHeight="normal">
+								{status}
+							</Box>
+						</ChakraButton>
+					) : (
+						<Button size={ButtonSize.SM} onClick={onConfirm}>
+							{status}
+						</Button>
+					)}
+				</Flex>
+			</ModalFooter>
+		</Modal>
+	)
 }
 
 export default DeactivateModal
