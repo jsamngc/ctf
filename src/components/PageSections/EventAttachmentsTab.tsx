1b103af6e6ecd5d0becd2c21f630c31d4bf98d13
Phong <pvo5@gmu.edu>
2020-09-10 12:25:40 -0400

    fixed data persistence and re-rendering upon replace

--- src/components/PageSections/EventAttachmentsTab.tsx
+++ src/components/PageSections/EventAttachmentsTab.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react"
-import { Box, Flex, Grid } from "@chakra-ui/core"
-import { FinePrint, P, H3, FileUploader } from "@c1ds/components"
-import AttachmentCard from "../AttachmentCard"
+import React, { useState, useRef } from "react"
+import { Box, Grid, useDisclosure, Flex, Divider, VisuallyHidden } from "@chakra-ui/core"
+import { FinePrint, P, H3, FileUploader, Link, Card } from "@c1ds/components"
+import { MoreVertSharp } from "@material-ui/icons"
+import { useSavedForm } from "../Utility/formHelpers"
+import DeleteFileModal from "../Modals/DeleteFileModal"
+import Dropdown from "../Dropdown"
 
 interface AttachmentTabProps {
 	eventData: EventFormData
@@ -11,6 +14,10 @@ interface AttachmentTabProps {
 export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabProps) => {
 	const { eventData, setEventData } = p
 	const { attachments } = eventData
+
+	const { isOpen: isAttachmentOpen, onOpen: onAttachmentOpen, onClose: onAttachmentClose } = useDisclosure()
+
+	const [savedEvents, updateSavedEvents] = useSavedForm<EventFormData[]>("ctfForms", "events")
 
 	// 1.6 The system displays appropriate error message when the selected file is in the unacceptable format.
 	// 1.7 The system displays appropriate error message when the file size exceeding 5MB
@@ -34,6 +41,148 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 		"application/rtf",
 		"application/pdf",
 	]
+
+	const maxSizeBytes = 1024 * 1024 * 5
+	const acceptedFileExtensions = [".jpg", ".jpeg", ".gif", ".png", ".xls", ".xlsx", ".doc", ".docx", ".txt", ".rtf", ".pdf"]
+	const attachmentRef = useRef<HTMLInputElement>(null)
+	const cardOptions = [
+		{
+			label: "Replace",
+			value: "Replace",
+			onClick: () => {
+				attachmentRef.current?.click()
+			},
+		},
+		{
+			label: "Remove",
+			value: "Remove",
+			onClick: () => {
+				onAttachmentOpen()
+			},
+		},
+	]
+	interface AttachmentCard {
+		attachmentDto: AttachmentDto
+	}
+
+	const AttachmentCard: React.FC<AttachmentCard> = (p: AttachmentCard) => {
+		const { attachmentDto } = p
+		const { attachments } = eventData
+		const attachmentRef = useRef<HTMLInputElement>(null)
+
+		const setFileOnCard = (file: File) => {
+			const reader = new FileReader()
+			reader.readAsDataURL(file)
+			reader.onload = () => {
+				if (eventData && eventData.attachments) {
+					eventData.attachments[eventData.attachments.indexOf(attachmentDto)] = {
+						fileName: file.name,
+						fileSize: file.size,
+						fileMimeType: file.type,
+						fileDataURL: reader.result,
+					}
+					setAttachmentDtoList([...eventData.attachments])
+					setEventData(eventData)
+					const savedEventIndex = savedEvents.findIndex((evt: EventFormData) => evt.eventId === eventData.eventId)
+					savedEvents.splice(savedEventIndex, 1, eventData)
+					updateSavedEvents(savedEvents)
+				}
+			}
+		}
+
+		return (
+			<Box>
+				<Card id="attachmentCard" maxWidth="full">
+					<Flex w="full" my={{ base: "-8px", sm: "-12px" }} flexDir={{ base: "row" }}>
+						{/* 1.4 The user can see the following information on the Attachment card
+					· Filename name (hyperlinked)
+					· [Replace] Function
+					· [Remove] function */}
+						{/* 1.5 The user can click on the hyperlinked Attachment filename to download the file and view the content. */}
+						<Flex flexGrow={1} justifyContent="flex-start">
+							<Link href={`${attachmentDto.fileDataURL}`} download={attachmentDto.fileName}>
+								{attachmentDto.fileName}
+							</Link>
+						</Flex>
+						<Flex display={{ base: "none", md: "flex" }} flexGrow={1} justifyContent="flex-end">
+							<Link
+								onClick={e => {
+									e.preventDefault()
+									setErrorMsg("")
+									attachmentRef.current?.click()
+								}}>
+								Replace
+							</Link>
+							<Box mx={24}>
+								<Divider
+									position="absolute"
+									h={{ base: 16, sm: 16 }}
+									top={{ base: -12, sm: -16 }}
+									orientation="vertical"
+									color="silver"
+								/>
+							</Box>
+							<Link
+								onClick={e => {
+									e.preventDefault()
+									onAttachmentOpen()
+								}}>
+								Remove
+							</Link>
+
+							<DeleteFileModal
+								type="attachment"
+								isOpen={isAttachmentOpen}
+								onCancel={onAttachmentClose}
+								onConfirm={() => {
+									const selectedIndex = attachments?.indexOf(attachmentDto)
+									eventData.attachments = attachments?.filter((att, index) => index !== selectedIndex)
+									setAttachmentDtoList(eventData.attachments ?? [])
+									setEventData(eventData)
+									const savedEventIndex = savedEvents.findIndex(
+										(evt: EventFormData) => evt.eventId === eventData.eventId
+									)
+									savedEvents.splice(savedEventIndex, 1, eventData)
+									updateSavedEvents(savedEvents)
+									onAttachmentClose()
+								}}
+							/>
+						</Flex>
+
+						<Flex display={{ md: "none" }} justifyContent="flex-end">
+							<Box position="relative" right={{ base: "-12px" }}>
+								<Dropdown
+									options={cardOptions}
+									borderedRows={true}
+									width="10rem"
+									label={`Additional actions for ${attachmentDto.fileName}`}>
+									<Box as={MoreVertSharp} color="clickable" />
+								</Dropdown>
+							</Box>
+						</Flex>
+					</Flex>
+				</Card>
+
+				<VisuallyHidden
+					as="input"
+					/* @ts-ignore */
+					type="file"
+					ref={attachmentRef}
+					name="talkingPoints"
+					aria-hidden={true}
+					tabIndex={-1}
+					accept={acceptedFileFormats.join()}
+					onChange={(e: React.FormEvent<HTMLInputElement>) => {
+						// @ts-ignore
+						const fileToReplace = e.target.files[0]
+						!acceptedFileFormats.includes(fileToReplace.type)
+							? setErrorMsg("File type must be one of .jpg .jpeg .gif .png .xls .xlsx .doc .docx .txt .rtf .pdf")
+							: setFileOnCard(fileToReplace)
+					}}
+				/>
+			</Box>
+		)
+	}
 
 	return (
 		<>
@@ -64,20 +213,8 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 					{/* 1.4.2 (Browse Function) The user can click [Drop/Browse] area to launch an interface (e.g. File Explorer) which allows the user to identify the proper document to be uploaded. */}
 					<FileUploader
 						id="attachments"
-						maxSizeBytes={1024 * 1024 * 5}
-						allowedFileTypes={[
-							".jpg",
-							".jpeg",
-							".gif",
-							".png",
-							".xls",
-							".xlsx",
-							".doc",
-							".docx",
-							".txt",
-							".rtf",
-							".pdf",
-						]}
+						maxSizeBytes={maxSizeBytes}
+						allowedFileTypes={acceptedFileExtensions}
 						progress={progress}
 						onDrop={(e, files) => {
 							setProgress(0)
@@ -92,11 +229,16 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 									fileMimeType: fileToSet.type,
 									fileDataURL: reader.result,
 								}
+								eventData.attachments?.push(attachment)
 								setAttachmentDtoList(currentAttachments => {
 									return [...currentAttachments, attachment]
 								})
-								eventData.attachments?.push(attachment)
 								setEventData(eventData)
+								const savedEventIndex = savedEvents.findIndex(
+									(evt: EventFormData) => evt.eventId === eventData.eventId
+								)
+								savedEvents.splice(savedEventIndex, 1, eventData)
+								updateSavedEvents(savedEvents)
 							}
 							setProgress(100)
 							setErrorMsg("")
@@ -114,14 +256,7 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 				{attachmentDtoList.map((value: AttachmentDto, index: number) => {
 					return (
 						<Box key={index} gridColumn="1 / -1">
-							<AttachmentCard
-								attachmentDto={value}
-								eventData={eventData}
-								setAttachmentDtoList={setAttachmentDtoList}
-								setEventData={setEventData}
-								setErrorMsg={setErrorMsg}
-								acceptedFileFormats={acceptedFileFormats}
-							/>
+							<AttachmentCard attachmentDto={value} />
 						</Box>
 					)
 				})}
