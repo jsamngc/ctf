f0c6151435216bbcc52f56e24f55692fa8da9c86
cw-infinite <deadmeats@hotmail.com>
2020-09-24 16:01:34 -0400

    Bug Fix #57 Partial Fix

--- src/components/PageSections/EventAttachmentsTab.tsx
+++ src/components/PageSections/EventAttachmentsTab.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Box, Grid, Flex } from "@chakra-ui/core"
+import { Box, Grid, Flex, Button } from "@chakra-ui/core"
 import { FinePrint, P, H3, FileUploader } from "@c1ds/components"
 import AttachmentCard from "../AttachmentCard"
 import { useCTFFormContextWSavedForm } from "../Forms/Form"
@@ -13,19 +13,17 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 	const { attachments } = eventData
 
 	const { savedForm: savedEvents, updateSavedForm: updateSavedEvents } = useCTFFormContextWSavedForm()
-
-	// 1.6 The system displays appropriate error message when the selected file is in the unacceptable format.
-	// 1.7 The system displays appropriate error message when the file size exceeding 5MB
 	const [errorMsg, setErrorMsg] = useState<string>("")
 	const [progress, setProgress] = useState(0)
 	const [attachmentDtoList, setAttachmentDtoList] = useState(attachments ?? [])
+
+	console.log(errorMsg)
 
 	const maxSizeBytes = 1024 * 1024 * 5
 	const acceptedFileExtensions = [".jpg", ".jpeg", ".gif", ".png", ".xls", ".xlsx", ".doc", ".docx", ".txt", ".rtf", ".pdf"]
 
 	return (
 		<>
-			{/*1.1 The user can access the Attachment screen from Edit Event option.*/}
 			<Grid
 				gridGap={{ base: "16px", md: "24px" }}
 				gridTemplateColumns={{
@@ -44,18 +42,12 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 					<Box mb={4}>
 						<FinePrint color="label">Attachments</FinePrint>
 					</Box>
-					{/* 1.2 The user can see the following section on the Attachment screen when there is no attachments.
-						· Upload File Area: [Drop/Browse] */}
-					{/* 1.6 The system displays the Upload File Area: [Drop/Browse] always on top of the file list */}
-					{/* 1.4 The user can upload file by using the Upload File Area: [Drop/Browse] */}
-					{/* 1.4.1 (Drop function) The user can drag a file from user’s local desktop and drop it into the [Drop/Browse] area to upload the file */}
-					{/* 1.4.2 (Browse Function) The user can click [Drop/Browse] area to launch an interface (e.g. File Explorer) which allows the user to identify the proper document to be uploaded. */}
 					<FileUploader
 						id="attachments"
 						maxSizeBytes={maxSizeBytes}
 						allowedFileTypes={acceptedFileExtensions}
 						progress={progress}
-						onDrop={(e, files) => {
+						onDrop={(_, files) => {
 							setProgress(0)
 							const fileToSet = files[0]
 							const reader = new FileReader()
@@ -82,19 +74,18 @@ export const AttachmentsTab: React.FC<AttachmentTabProps> = (p: AttachmentTabPro
 						handleRejectedFiles={(_, files) => {
 							setErrorMsg(files[0].errors[0])
 						}}
+						errorMessage={errorMsg}
 					/>
-					{errorMsg !== "" && <P color="error">{errorMsg}</P>}
 				</Box>
 
-				{/* 1.3 The user can see the following section on the Attachment screen when there are pre-existing attachments
-				· Upload File Area: [Drop/Browse]
-				· Attachment Card */}
 				{attachmentDtoList.map((value: AttachmentDto, index: number) => {
 					return (
 						<Box key={index} gridColumn="1 / -1">
 							<AttachmentCard
 								eventData={eventData}
 								setErrorMsg={setErrorMsg}
+								setProgress={setProgress}
+								maxSize={maxSizeBytes}
 								setAttachmentDtoList={setAttachmentDtoList}
 								attachmentDto={value}
 							/>
